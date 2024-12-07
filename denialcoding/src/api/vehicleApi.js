0bfb135e590f48fb343ad459ef6f31d312df1450
Denialenial <motebang763@gmail.com>
2024-12-06 14:48:47 -1200

    api solutions

--- denialcoding/src/api/vehicleApi.js
+++ denialcoding/src/api/vehicleApi.js
@@ -1,39 +1,40 @@
-import axios from 'axios';
+import axios from 'axios';  
 
-const API_URL = 'http://localhost:5003';
+// Use a dynamic API_URL based on environment  
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5003';  
 
-export const getVehicles = async () => {
-    try {
-        const response = await axios.get(`${API_URL}/vehicles`);
-        return response.data;
-    } catch (error) {
-        console.error('There was an error fetching the vehicles!', error);
-    }
-};
+export const getVehicles = async () => {  
+    try {  
+        const response = await axios.get(`${API_URL}/vehicles`);  
+        return response.data;  
+    } catch (error) {  
+        console.error('There was an error fetching the vehicles!', error);  
+    }  
+};  
 
-export const addVehicle = async (vehicle) => {
-    try {
-        const response = await axios.post(`${API_URL}/vehicles`, vehicle);
-        return response.data;
-    } catch (error) {
-        console.error('There was an error adding the vehicle!', error);
-    }
-};
+export const addVehicle = async (vehicle) => {  
+    try {  
+        const response = await axios.post(`${API_URL}/vehicles`, vehicle);  
+        return response.data;  
+    } catch (error) {  
+        console.error('There was an error adding the vehicle!', error);  
+    }  
+};  
 
-export const updateVehicle = async (id, vehicle) => {
-    try {
-        const response = await axios.put(`${API_URL}/vehicles/${id}`, vehicle);
-        return response.data;
-    } catch (error) {
-        console.error('There was an error updating the vehicle!', error);
-    }
-};
+export const updateVehicle = async (id, vehicle) => {  
+    try {  
+        const response = await axios.put(`${API_URL}/vehicles/${id}`, vehicle);  
+        return response.data;  
+    } catch (error) {  
+        console.error('There was an error updating the vehicle!', error);  
+    }  
+};  
 
-export const deleteVehicle = async (id) => {
-    try {
-        const response = await axios.delete(`${API_URL}/vehicles/${id}`);
-        return response.data;
-    } catch (error) {
-        console.error('There was an error deleting the vehicle!', error);
-    }
+export const deleteVehicle = async (id) => {  
+    try {  
+        const response = await axios.delete(`${API_URL}/vehicles/${id}`);  
+        return response.data;  
+    } catch (error) {  
+        console.error('There was an error deleting the vehicle!', error);  
+    }  
 };
