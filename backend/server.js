ea05ab6449fb4d8e3aa6f1e366ec0a8ba08c53b6
Denialenial <motebang763@gmail.com>
2024-12-06 12:41:37 -1200

    Enable CORS for all routes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5003; // Change the port number here
 
 // Middleware
-app.use(cors());
+app.use(cors()); // Enable CORS for all routes
 app.use(bodyParser.json());
 
 // Connect Database
