f453f92aeb9113ecdbd2f7b989e6555c136e52ef
Denialenial <motebang763@gmail.com>
2024-12-06 19:42:12 -1200

    server improvemnet

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,26 @@
-const express = require('express');  
-const mongoose = require('mongoose');  
-const cors = require('cors');  
-const bodyParser = require('body-parser');  
-const employeeRoutes = require('./routes/employees');  
-const vehicleRoutes = require('./routes/vehicles');  
-const connectDB = require('./mongoose'); // Import the connectDB function  
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const employeeRoutes = require('./routes/employees');
+const vehicleRoutes = require('./routes/vehicles');
+const connectDB = require('./mongoose'); // Import the connectDB function
 
-const app = express();  
-const PORT = process.env.PORT || 5003; // Change the port number here  
+const app = express();
+const PORT = process.env.PORT || 5004; // Change the port number here
 
-// Middleware  
-app.use(cors({  
-    origin: 'https://hr-managementboth.vercel.app', // Allow your frontend domain  
-}));  
-app.use(bodyParser.json());  
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
 
-// Connect Database  
-connectDB();  
+// Connect Database
+connectDB();
 
-// Routes  
-app.use('/employees', employeeRoutes);  
-app.use('/vehicles', vehicleRoutes);  
+// Routes
+app.use('/employees', employeeRoutes);
+app.use('/vehicles', vehicleRoutes);
 
-// Start server  
-app.listen(PORT, () => {  
-    console.log(`Server running on port ${PORT}`);  
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
